dcd3147dccb29b089a0eeffbe11de0fc7735cf02
Johee Chung <johee.chung@gmail.com>
2017-04-12 10:18:51 -0500

    Change components to export function instead of class

--- src/components/card-form/index.jsx
+++ src/components/card-form/index.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
 
-export default class AddCard extends React.Component {
-  render() {
-    const addCard = this.props.addCard;
-    let name;
-    let photo;
-    return(
-      <form>  
-        <div>
-          <label>Full Name</label>
-          <input ref={ node => { name = node; }} required></input>
-        </div>
-        <div>
-          <label>Photo URL</label>
-          <input ref={ node => { photo = node; }}></input> 
-        </div>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            addCard({
-              name: name.value,
-              photo: photo.value
-            });
-            name.value = '';
-            photo.value = '';
-          }}
-        >Create</button>
-      </form>
-    );
-  }
+export default (props) => {
+  const addCard = props.addCard;
+  let name;
+  let photo;
+  return(
+    <form>  
+      <div>
+        <label>Full Name</label>
+        <input ref={ node => { name = node; }} required></input>
+      </div>
+      <div>
+        <label>Photo URL</label>
+        <input ref={ node => { photo = node; }}></input> 
+      </div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          addCard({
+            name: name.value,
+            photo: photo.value
+          });
+          name.value = '';
+          photo.value = '';
+        }}
+      >Create</button>
+    </form>
+  );
 }
--- src/components/card-list/index.jsx
+++ src/components/card-list/index.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import './style.scss';
 
-export default class Cards extends React.Component {
-  render() {
-    const cards = this.props.cards;
-    const removeCard = this.props.removeCard;
-    return(
-      <div className='card-list'>
-        {cards.map(card => 
-          <div key={card.id} className='card'>
-            <button className="close-button" onClick={() => removeCard(card.id)}>x</button>
-            <img src={card.photo} alt={card.name} height="42" width="42"/> 
-            <span>{card.name}</span>
-          </div>
-        )}
-      </div>
-    );
-  }
+export default (props) => {
+  const cards = props.cards;
+  const removeCard = props.removeCard;
+  return(
+    <div className='card-list'>
+      {cards.map(card => 
+        <div key={card.id} className='card'>
+          <button className="close-button" onClick={() => removeCard(card.id)}>x</button>
+          <img src={card.photo} alt={card.name} height="42" width="42"/> 
+          <span>{card.name}</span>
+        </div>
+      )}
+    </div>
+  );
 }
