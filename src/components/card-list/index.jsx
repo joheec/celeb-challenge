834e97cea656a425fa20f03468789e8d3ce1002c
Johee Chung <johee.chung@gmail.com>
2017-04-12 11:41:33 -0500

    Update close button

--- src/components/card-list/index.jsx
+++ src/components/card-list/index.jsx
@@ -4,11 +4,12 @@ import './style.scss';
 export default (props) => {
   const cards = props.cards;
   const removeCard = props.removeCard;
+  const defaultPhoto = "components/card-list/close.png";
   return(
     <div className='card-list'>
       {cards.map(card => 
         <div key={card.id} className='card'>
-          <img src="./close.png" className="close-button" onClick={() => removeCard(card.id)}></img>
+          <img src={defaultPhoto} className="close-button" onClick={() => removeCard(card.id)}></img>
           <img src={card.photo} alt={card.name} className="celeb-photo" /> 
           <span className="celeb-name">{card.name}</span>
         </div>
